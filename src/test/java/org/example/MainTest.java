1d121c3c30cf029f3ec6de4560f86300174a3b24
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-09-11 08:21:11 +0700

    test class created

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,12 +2,33 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void inc() {
-        assertEquals(2, Main.inc(1));
+    void sequence() {
+        int n = 4;
+        List<String> sequence = Main.sequence(n);
+        List<String> expected = new  ArrayList<>();
+        expected.add("1");
+        expected.add("2");
+        expected.add("3");
+        expected.add("4");
+        assertEquals(expected, sequence);
+
+        n = 5;
+        expected.add("fizz");
+        assertEquals(expected, Main.sequence(n));
+    }
+
+    @Test
+    void sequenceNegativeSize() {
+        int n = -2;
+        List<String> sequence = Main.sequence(n);
+
+        assertNull(sequence);
     }
 }
